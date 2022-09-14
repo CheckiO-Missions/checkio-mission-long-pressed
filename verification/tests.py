2f7da7b37f4bdddd23eb8888ae8756f7d1b95c2a
Alexander Blaschuk <blaschuk95@gmail.com>
2022-09-14 16:13:45 +0300

    create

--- verification/tests.py
+++ verification/tests.py
@@ -14,7 +14,7 @@ TESTS = {
             "answer": True
         },
         {
-            "input": ["welcome to Checkio", "weeeelcome to cccheckio"],
+            "input": ["welcome to сheckio", "weeeelcome to cccheckio"],
             "answer": True
         },
         {
